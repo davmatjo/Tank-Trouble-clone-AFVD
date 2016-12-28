bdf4cd005500fa781fc56eafd515057a00d5c9e5
David <davmatjo@gmail.com>
2016-12-28 19:20:00 +0000

    Started on help menu

--- Title_Screen.py
+++ Title_Screen.py
@@ -37,11 +37,12 @@ def title_screen():
         button("1 Player", 250, 300, 300, 60)
         button("2 Players", 250, 400, 300, 60, start_2_player)
         button("3 Players", 250, 500, 300, 60, start_3_player)
+        button("Help", 680, 720, 100, 60, help_screen)
 
         #Text
-        text_surf1, text_rect1 = text_objects("Armoured Fighting", pygame.font.Font('arial.ttf', 70))
+        text_surf1, text_rect1 = text_objects("Armoured Fighting", pygame.font.SysFont('comicsansms', 70))
         text_rect1.center = ((400, 60))
-        text_surf2, text_rect2 = text_objects("Vehicle Dissatisfaction", pygame.font.Font('arial.ttf', 70))
+        text_surf2, text_rect2 = text_objects("Vehicle Dissatisfaction", pygame.font.SysFont('comicsansms', 70))
         text_rect2.center = ((400, 140))
         screen.blit(text_surf1, text_rect1)
         screen.blit(text_surf2, text_rect2)
@@ -68,6 +69,15 @@ def button(text, x, y, width, height, action=None):
 def text_objects(text, font):
     textSurface = font.render(text, True, (0, 0, 0))
     return textSurface, textSurface.get_rect()
+
+
+def help_screen():
+    help_on = True
+    while help_on:
+        screen.blit(pygame.image.load("help.png", (0, 0)).convert())
+        if pygame.key.get_pressed()[K_ESCAPE]:
+            help_on = False
+
 
 
 title_screen()
