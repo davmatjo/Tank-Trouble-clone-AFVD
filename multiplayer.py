142a5197c96b24ea301ca8195b8359dd3e83dfb8
David <davmatjo@gmail.com>
2016-12-29 15:59:33 +0000

    Spawnpoint now randomly selected from available list some further reduction of code in main game loop

--- multiplayer.py
+++ multiplayer.py
@@ -6,6 +6,7 @@ from Bullet import Bullet
 from Tank import Tank
 from sincos import degcos
 from sincos import degsin
+from random import randrange
 
 window_size = (800, 800)
 FPS = 60
@@ -46,15 +47,41 @@ def player_3_movement(afv):
         afv.forward()
 
 
+def get_spawnpoints(maze_size, player_count):
+    block_size = (window_size[0] / maze_size)
+    possible_spawnpoints = []
+    tank_positions = []
+    y_pointer = -(block_size / 2)
+    for i in range(maze_size):
+        x_pointer = -(block_size / 2)
+        y_pointer += block_size
+        for j in range(maze_size):
+            x_pointer += block_size
+            possible_spawnpoints.append([x_pointer, y_pointer])
+
+    print(possible_spawnpoints)
+    for i in range(player_count):
+        tank_coordinates = randrange(0, len(possible_spawnpoints) - 1)
+        print(tank_coordinates)
+        tank_coordinates = possible_spawnpoints.pop(tank_coordinates)
+        print(tank_coordinates)
+        tank_positions.append(tank_coordinates)
+    return tank_positions
+
+
 def start_2_player(screen):
     class MainGame:
         """Class that handles some aspects of running the game, such as performing the regular calculations needed for
                 every refresh"""
         def __init__(self):
             self.game = True
+            self.maze_size = 7
+            self.new_maze()
+            self.tanks = []
+            self.create_players()
 
         def new_maze(self):
-            my_maze = Maze(7)
+            my_maze = Maze(self.maze_size)
             my_maze.generate_maze()
             my_maze.render_maze()
             maze_image = pygame.image.load("maze.png").convert()
@@ -62,22 +89,25 @@ def start_2_player(screen):
 
         def refresh(self):
             screen.blit(self.background, (0, 0))
-            for tank in tanks:
+            for tank in self.tanks:
                 tank.move()
                 tank.draw()
                 tank.velocity = [0, 0]
-            for bullet in tanks[0].fired_bullets:
+            for bullet in self.tanks[0].fired_bullets:
                 bullet.move()
                 bullet.draw()
                 bullet.lifespan()
-                if not tanks[0].fired_bullets[0].alive:
-                    tanks[0].fired_bullets.pop(0)
-            for bullet in tanks[1].fired_bullets:
+                if not self.tanks[0].fired_bullets[0].alive:
+                    self.tanks[0].fired_bullets.pop(0)
+            for bullet in self.tanks[1].fired_bullets:
                 bullet.move()
                 bullet.draw()
                 bullet.lifespan()
-                if not tanks[1].fired_bullets[0].alive:
-                    tanks[1].fired_bullets.pop(0)
+                if not self.tanks[1].fired_bullets[0].alive:
+                    self.tanks[1].fired_bullets.pop(0)
+            self.handle_inputs()
+            player_1_movement(self.tanks[0])
+            player_2_movement(self.tanks[1])
 
         def handle_inputs(self):
             """
@@ -89,30 +119,31 @@ def start_2_player(screen):
                     sys.exit()
                 if event.type == KEYDOWN:
                     if event.key == K_e:
-                        if len(tanks[0].fired_bullets) < 10:
+                        if len(self.tanks[0].fired_bullets) < 10:
                             print("Fire!")
-                            tanks[0].fired_bullets.append(Bullet(screen, [tanks[0].position[0], tanks[0].position[1]],
-                                                     [2 * degcos(tanks[0].angle), -2 * degsin(tanks[0].angle)]))
+                            self.tanks[0].fired_bullets.append(Bullet(screen, [self.tanks[0].position[0], self.tanks[0].position[1]],
+                                                                      [2 * degcos(self.tanks[0].angle), -2 * degsin(
+                                                                     self.tanks[0].angle)]))
                     if event.key == K_KP0:
-                        if len(tanks[1].fired_bullets) < 10:
+                        if len(self.tanks[1].fired_bullets) < 10:
                             print("Fire!")
-                            tanks[1].fired_bullets.append(Bullet(screen, [tanks[1].position[0], tanks[1].position[1]],
-                                                     [2 * degcos(tanks[1].angle), -2 * degsin(tanks[1].angle)]))
+                            self.tanks[1].fired_bullets.append(Bullet(screen, [self.tanks[1].position[0], self.tanks[1].position[1]],
+                                                                      [2 * degcos(self.tanks[1].angle), -2 * degsin(
+                                                                     self.tanks[1].angle)]))
                     if event.key == K_ESCAPE:
                         game.game = False
+
+        def create_players(self):
+            tank_positions = get_spawnpoints(self.maze_size, 2)
+            self.tanks.append(Tank(screen, tank_positions[0], "Player 1", "Assets/AFV1.png"))
+            self.tanks.append(Tank(screen, tank_positions[1], "Player 2", "Assets/AFV2.png"))
 
 
     # initialising of all arrays and objects preparing for game
     game = MainGame()
-    game.new_maze()
-    tanks = []
-    tanks.append(Tank(screen, [500, 500], "Player 1", "Assets/AFV1.png"))
-    tanks.append(Tank(screen, [270, 250], "Player 2", "Assets/AFV2.png"))
+
     # main game loop
     while game.game:
-        game.handle_inputs()
-        player_1_movement(tanks[0])
-        player_2_movement(tanks[1])
         game.refresh()
         pygame.display.update()
         fps_clock.tick(FPS)
@@ -126,9 +157,13 @@ def start_3_player(screen):
                 every refresh"""
         def __init__(self):
             self.game = True
+            self.maze_size = 8
+            self.new_maze()
+            self.tanks = []
+            self.create_players()
 
         def new_maze(self):
-            my_maze = Maze(8)
+            my_maze = Maze(self.maze_size)
             my_maze.generate_maze()
             my_maze.render_maze()
             maze_image = pygame.image.load("maze.png").convert()
@@ -136,67 +171,73 @@ def start_3_player(screen):
 
         def refresh(self):
             screen.blit(self.background, (0, 0))
-            for tank in tanks:
+            for tank in self.tanks:
                 tank.move()
                 tank.draw()
                 tank.velocity = [0, 0]
-            for bullet in tanks[0].fired_bullets:
+            for bullet in self.tanks[0].fired_bullets:
                 bullet.move()
                 bullet.draw()
                 bullet.lifespan()
-                if not tanks[0].fired_bullets[0].alive:
-                    tanks[0].fired_bullets.pop(0)
-            for bullet in tanks[1].fired_bullets:
+                if not self.tanks[0].fired_bullets[0].alive:
+                    self.tanks[0].fired_bullets.pop(0)
+            for bullet in self.tanks[1].fired_bullets:
                 bullet.move()
                 bullet.draw()
                 bullet.lifespan()
-                if not tanks[1].fired_bullets[0].alive:
-                    tanks[1].fired_bullets.pop(0)
-            for bullet in tanks[2].fired_bullets:
+                if not self.tanks[1].fired_bullets[0].alive:
+                    self.tanks[1].fired_bullets.pop(0)
+            for bullet in self.tanks[2].fired_bullets:
                 bullet.move()
                 bullet.draw()
                 bullet.lifespan()
-                if not tanks[2].fired_bullets[0].alive:
-                    tanks[2].fired_bullets.pop(0)
+                if not self.tanks[2].fired_bullets[0].alive:
+                    self.tanks[2].fired_bullets.pop(0)
+            self.handle_inputs()
 
         def handle_inputs(self):
             """
-            Contains additional inputs for third player
+            Input handler for all players
             :return:
             """
+            player_1_movement(self.tanks[0])
+            player_2_movement(self.tanks[1])
+            player_3_movement(self.tanks[2])
             for event in pygame.event.get():
                 if event.type == QUIT:
                     sys.exit()
                 if event.type == KEYDOWN:
                     if event.key == K_e:
-                        if len(tanks[0].fired_bullets) < 10:
+                        if len(self.tanks[0].fired_bullets) < 10:
                             print("Fire!")
-                            tanks[0].fired_bullets.append(Bullet(screen, [tanks[0].position[0], tanks[0].position[1]],
-                                                     [2 * degcos(tanks[0].angle), -2 * degsin(tanks[0].angle)]))
+                            self.tanks[0].fired_bullets.append(Bullet(screen, [self.tanks[0].position[0], self.tanks[0].position[1]],
+                                                                      [2 * degcos(self.tanks[0].angle), -2 * degsin(
+                                                                     self.tanks[0].angle)]))
                     if event.key == K_KP0:
-                        if len(tanks[1].fired_bullets) < 10:
+                        if len(self.tanks[1].fired_bullets) < 10:
                             print("Fire!")
-                            tanks[1].fired_bullets.append(Bullet(screen, [tanks[1].position[0], tanks[1].position[1]],
-                                                     [2 * degcos(tanks[1].angle), -2 * degsin(tanks[1].angle)]))
+                            self.tanks[1].fired_bullets.append(Bullet(screen, [self.tanks[1].position[0], self.tanks[1].position[1]],
+                                                                      [2 * degcos(self.tanks[1].angle), -2 * degsin(
+                                                                     self.tanks[1].angle)]))
                     if event.key == K_o:
-                        if len(tanks[2].fired_bullets) < 10:
+                        if len(self.tanks[2].fired_bullets) < 10:
                             print("Fire!")
-                            tanks[2].fired_bullets.append(Bullet(screen, [tanks[2].position[0], tanks[2].position[1]],
-                                                     [2 * degcos(tanks[2].angle), -2 * degsin(tanks[2].angle)]))
+                            self.tanks[2].fired_bullets.append(Bullet(screen, [self.tanks[2].position[0], self.tanks[2].position[1]],
+                                                                      [2 * degcos(self.tanks[2].angle), -2 * degsin(
+                                                                     self.tanks[2].angle)]))
                     if event.key == K_ESCAPE:
                         game.game = False
 
+        def create_players(self):
+            tank_positions = get_spawnpoints(self.maze_size, 3)
+            self.tanks.append(Tank(screen, tank_positions[0], "Player 1", "Assets/AFV1.png"))
+            self.tanks.append(Tank(screen, tank_positions[1], "Player 2", "Assets/AFV2.png"))
+            self.tanks.append(Tank(screen, tank_positions[2], "Player 3", "Assets/AFV1.png"))
+
+
     game = MainGame()
-    game.new_maze()
-    tanks = []
-    tanks.append(Tank(screen, [550, 550], "Player 1", "Assets/AFV1.png"))
-    tanks.append(Tank(screen, [270, 250], "Player 2", "Assets/AFV2.png"))
-    tanks.append(Tank(screen, [100, 750], "Player 3", "Assets/AFV1.png"))
+
     while game.game:
-        game.handle_inputs()
-        player_1_movement(tanks[0])
-        player_2_movement(tanks[1])
-        player_3_movement(tanks[2])
         game.refresh()
         pygame.display.update()
         fps_clock.tick(FPS)
